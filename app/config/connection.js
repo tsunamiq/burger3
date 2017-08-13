2f8aa1eeec17b2e7440696fa0f7cd3a02d391863
Kevin Brannan <brannake@gmail.com>
2017-08-12 18:40:15 -0700

    fixes it up enough to run through presentation

--- app/config/connection.js
+++ app/config/connection.js
@@ -6,9 +6,9 @@ if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'TANNERsucks26!!',
+    host: 'mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'b3xedheg0xq5zztq',
+    password: 'zucv0erircyg0c1c',
     database: 'burger3_db'
   });
 };
--- app/public/javascript/main.js
+++ app/public/javascript/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    startMenuCall();
+    grabsUserData();
 //Grabs user data and appends it to the page
 function grabsUserData() {
       var user_id = parseInt(localStorage.getItem("user_id"));
@@ -8,7 +10,6 @@ function grabsUserData() {
     }).done(function(response) {
       
         var userArrayIndex = 0;
-
         console.log("==================================================")
         console.log("user ID from local storage: " + user_id)
         console.log("==================================================")
@@ -23,33 +24,16 @@ function grabsUserData() {
                 
             }
         }
-
         console.log("==================================================")
         console.log("userArrayIndex: " + userArrayIndex)
-
         //diplay user calor info
         $("#user-name-1").text(response[userArrayIndex].first_name +" "+ response[userArrayIndex].last_name);
-
         console.log("User information: ");
         console.log(response[userArrayIndex]);
-
-        //calories info fixed
-        var calorieNew = response[userArrayIndex].calorieNew;
-        console.log("New Calories: " + calorieNew)
-
-        $("#calories-fixed-value").text(calorieNew).attr("value",calorieNew)
-        userConsumption();
     });
 };
-
-
-
-
-
 //Attaches on-click events for menu items
-
 function startMenuCall () {
-        var user_id = parseInt(localStorage.getItem("user_id"));
         $.get({
             url: "/api/restaurant",
             method: "GET"
@@ -57,7 +41,6 @@ function startMenuCall () {
             console.log(response);
             console.log(response[0].restaurant);
             for (i=0; i < 300; i = i +25) {
-
                 //Creates the display container and adds all the columns
                 //to contain the data for that entry/item
                 var restaurantDiv = $("<div></div>");
@@ -77,14 +60,12 @@ function startMenuCall () {
                 $(priceColumn).attr("class", "col s2");
                 var locationColumn = $("<div></div>");
                 $(locationColumn).attr("class", "col s2");
-
                 //Writes the data into each column
                 $(restaurantColumn).text(response[i].restaurant);
                 $(itemColumn).text(response[i].item);
                 $(caloriesColumn).text(response[i].calories);
                 $(priceColumn).text(response[i].price);
                 $(locationColumn).text("6265 Greenwich Ave");
-
                 //Appends each column to the display container div
                 $(restaurantDiv).append(spacerColumn);
                 $(restaurantDiv).append(restaurantColumn);
@@ -92,31 +73,33 @@ function startMenuCall () {
                 $(restaurantDiv).append(caloriesColumn);
                 $(restaurantDiv).append(priceColumn);
                 $(restaurantDiv).append(locationColumn);
+                console.log(restaurantDiv);
                 
                 //Adds price and calorie values to each div
                 $(restaurantDiv).attr("calories", response[i].calories);
                 $(restaurantDiv).attr("price", response[i].price);
                 $(restaurantDiv).attr("item", response[i].item);
                 $(restaurantDiv).attr("restaurant", response[i].item);
-
+                $("#search-submit-shadow").append(restaurantDiv);
                 //Adds on-click events to each div
                 $(restaurantDiv).on("click",function(){
                     var calorieValue = parseInt($(this).attr("calories"));
+                    console.log(calorieValue);
                     var priceValue = parseInt($(this).attr("price"));
+                    console.log(priceValue);
                     var beforeCaloriesLeft = parseInt($("#calories-left-1").text());
                     var afterCaloriesLeft = beforeCaloriesLeft - calorieValue;
-
+                    console.log(afterCaloriesLeft);
                     if (afterCaloriesLeft < 0) {
                         var alertDiv = $("<div id='alertDiv'>That would put you over your calorie limit! Ahhh!!! Learn some self-control!!!!</div>");
                         $("body").append(alertDiv);
                         setTimeout(function() {$(alertDiv).css("visibility", "hidden")}, 2000)
                     } else {
                     $("#calories-left-1").text(afterCaloriesLeft);
+                    console.log(afterCaloriesLeft);
                     var beforeCaloriesConsumed = parseInt($("#user-calories-consumed-1").text());
                     var afterCaloriesConsumed = beforeCaloriesConsumed + calorieValue;
                     $("#user-calories-consumed-1").text(parseInt(afterCaloriesConsumed));
-
-
                     var itemName = $(this).attr("item");
                     var price = $(this).attr("price");
                     var itemNamediv = $("<div></div>");
@@ -124,74 +107,17 @@ function startMenuCall () {
                     itemNamediv.append($("<br>"));
                     itemNamediv.append(price);
                     $("#selected-item-display").append(itemNamediv);
-
-
                     var consumption = {
-                    user_id: user_id,
+                    user_id: 1,
                     food_name: itemName,
                     calorie: calorieValue,
                     cost: priceValue 
                     }
-                
-                    storeMealatConsumptionAPI(consumption);
-                    }
-
-                    userConsumption();
-                    // DISPLAY CONSUMPTION
-
-                
-
-        
-
-
-    })
              
-
                 //Appends the display container div to the page container
-                $("#search-submit-shadow").append(restaurantDiv);
-            }  
-        });
-    };
-
-
-
-
-
-
-function userConsumption(){
-    var user_id = parseInt(localStorage.getItem("user_id"));
-        $.get({
-            url: "/api/consumption/all/"+user_id,
-            method: "GET"
-        }).done(function(response) {
-
-            console.log("==========================================");
-            console.log("user Id: " + user_id)
-            console.log("user consumption:");
-            console.log(response);
-            console.log("==========================================");
-            var calorieSum = 0;
-            for(var i = 0; i<response.length ; i++){
-                calorieSum = calorieSum + response[0].calorie;
             }
-            var caloriesLeft = parseInt($("#calories-fixed-value").attr("value"))-calorieSum;
-
-            console.log("calories left: " + caloriesLeft);
-            $("#user-calories-consumed-1").text(calorieSum);
-            $("#calories-left-1").text(caloriesLeft);
+                })
+    }
         })
 }
-
-
-function storeMealatConsumptionAPI (consumption) {
-    $.post("/api/consumption/add", consumption)
-        .done(function(data) {
-            console.log(data);
-            });
-        };
-
-
-
-        startMenuCall();
-        grabsUserData();
 });
